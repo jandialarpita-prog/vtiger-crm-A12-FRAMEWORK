e7a11653557fcbb7fb5b0db4a77ceb6c9cfdfad3
hp <hp@DESKTOP-J8AE8VB>
2025-09-30 01:19:52 +0530

    Today's Work

---
+++ vtiger-CRM-FRAMEWORK-A12/src/test/java/ddt_extra/Generate_Random_Number.java
@@ -0,0 +1,11 @@
+package ddt_extra;
+
+public class Generate_Random_Number {
+
+	public static void main(String[] args) {
+		
+		int ran = (int)(Math.random()*10000);
+		
+		System.out.println(ran);
+	}
+}
--- vtiger-CRM-FRAMEWORK-A12/src/test/java/organization/CreateORGANIZATION.java
+++ vtiger-CRM-FRAMEWORK-A12/src/test/java/organization/CreateORGANIZATION.java
@@ -1,7 +1,15 @@
 package organization;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,107 +18,118 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class CreateORGANIZATION {
-public static void main(String[] args) throws InterruptedException {
-		
-		//OPEN BROWSER
-		WebDriver driver =new ChromeDriver();
+	public static void main(String[] args) throws InterruptedException, EncryptedDocumentException, IOException {
+
+		// Get the data from excel file
+
+		FileInputStream fisexcel = new FileInputStream("./src/test/resources/testScriptData.xlsx");
+
+		Workbook wb = WorkbookFactory.create(fisexcel);
+
+		Sheet sh = wb.getSheet("org");
+
+		Row row = sh.getRow(6);
+
+		Cell cell = row.getCell(0);
+
+		String orgName = cell.getStringCellValue() + (int) (Math.random() * 10000);
+
+		// OPEN BROWSER
+		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-		
-		//LOGIN
+
+		// LOGIN
 		driver.get("http://localhost:8888/");
-		
+
 		WebElement username = driver.findElement(By.name("user_name"));
 		username.sendKeys("admin");
-		
+
 		WebElement password = driver.findElement(By.name("user_password"));
 		password.sendKeys("manager");
-		
+
 		driver.findElement(By.id("submitButton")).click();
 		Thread.sleep(2000);
-		
-		//CREATE ORGANIZATION
+
+		// CREATE ORGANIZATION
 		driver.findElement(By.linkText("Organizations")).click();
-		
+
 		Thread.sleep(2000);
-		
+
 		driver.findElement(By.cssSelector("img[alt='Create Organization...']")).click();
-		
-		//FILLING DATA TO THE FORM
+
+		// FILLING DATA TO THE FORM
 		WebElement orgField = driver.findElement(By.name("accountname"));
-		String orgName = "automationwitharpita";
+		// String orgName = "automationwitharpita";
 		orgField.sendKeys(orgName);
-		
+
 		Thread.sleep(2000);
-		
+
 		WebElement phoneNumber = driver.findElement(By.id("phone"));
 		phoneNumber.sendKeys("9834751880");
-		
+
 		Thread.sleep(2000);
-		
+
 		WebElement mail = driver.findElement(By.id("email1"));
 		mail.sendKeys("arpitagupta@gmail.com");
 		Thread.sleep(2000);
-		
-		WebElement dropDown= driver.findElement(By.name("rating"));
+
+		WebElement dropDown = driver.findElement(By.name("rating"));
 		Select rating = new Select(dropDown);
 		rating.selectByValue("Active");
-		
-        Thread.sleep(2000);
-		
-        WebElement webSite = driver.findElement(By.cssSelector("input[name='website']"));
+
+		Thread.sleep(2000);
+
+		WebElement webSite = driver.findElement(By.cssSelector("input[name='website']"));
 		webSite.sendKeys("www.automationproject.in");
-		
+
 		Thread.sleep(2000);
-		
-        WebElement employ = driver.findElement(By.id("employees"));
+
+		WebElement employ = driver.findElement(By.id("employees"));
 		employ.sendKeys("45");
-		
+
 		Thread.sleep(2000);
-		
-		WebElement dropDown1= driver.findElement(By.name("industry"));
+
+		WebElement dropDown1 = driver.findElement(By.name("industry"));
 		Select industry = new Select(dropDown1);
 		industry.selectByValue("Banking");
-		
+
 		Thread.sleep(2000);
-		
-		WebElement dropDown2= driver.findElement(By.name("accounttype"));
+
+		WebElement dropDown2 = driver.findElement(By.name("accounttype"));
 		Select accounttype = new Select(dropDown2);
 		accounttype.selectByValue("Partner");
-		
+
 		Thread.sleep(2000);
-		
-		WebElement annualRevenue= driver.findElement(By.name("annual_revenue"));
+
+		WebElement annualRevenue = driver.findElement(By.name("annual_revenue"));
 		annualRevenue.sendKeys("6000000");
-		
+
 		Thread.sleep(2000);
-		
-		//SAVE
+
+		// SAVE
 		driver.findElement(By.cssSelector("input[title='Save [Alt+S]']")).click();
-		
-		//VERIFICATION
+
+		// VERIFICATION
 		String actOrgName = driver.findElement(By.id("dtlview_Organization Name")).getText();
-		
-		if(actOrgName .equals(orgName)) {
+
+		if (actOrgName.equals(orgName)) {
 			System.out.println("Created Organization Successfully!!!!");
-		}
-		else {
+		} else {
 			System.out.println("Failed......");
 		}
-		
+
 		WebElement profilePic = driver.findElement(By.cssSelector("img[src='themes/softed/images/user.PNG']"));
-		
+
 		Actions act = new Actions(driver);
-		
+
 		act.moveToElement(profilePic).build().perform();
-		
+
 		Thread.sleep(2000);
 		driver.findElement(By.linkText("Sign Out")).click();
-		
+
 		Thread.sleep(3000);
 		driver.quit();
 	}
-	
-
 
 }
